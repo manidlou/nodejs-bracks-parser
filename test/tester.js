8b2991501bdcebd95852c6eea904f4179d7050b0
mawni <mawni@gmail.com>
2016-05-02 13:32:16 -0700

    add more semi-complexed test cases

--- test/tester.js
+++ test/tester.js
@@ -11,7 +11,7 @@ describe("parse bracks-style html with semi-complex nested elements", function()
         throw err;
       } else if (ok) {
         bracks_html = fs.readFileSync(path.join(process.cwd(), 'test', 'views', 'index-html.html'), 'utf8');
-        expect(bracks_html.trim()).to.equal('<div id="divId" class="divclass"><ul style="list-style-type:disc"><li><a href="https://www.google.com"><span class="primary">link to google</span></a></li></ul></div>');
+        expect(bracks_html.trim()).to.equal('<html><meta charset="utf-8"> <body> <div id="divId" class="divclass"><ul style="list-style-type:disc"><li><a href="https://www.google.com"><span class="primary">link to google</span></a></li></ul></div></body></html>');
         done();
       }
     });
@@ -26,7 +26,7 @@ describe("parse bracks-style ejs with semi-complex nested elements", function() 
         throw err;
       } else if (ok) {
         bracks_ejs = fs.readFileSync(path.join(process.cwd(), 'test', 'views', 'index-ejs.ejs'), 'utf8');
-        expect(bracks_ejs.trim()).to.equal('<% include template/header.ejs %> <div id="divId" class="<%= page %>"><ul style="list-style-type:disc"><li><a href="https://www.google.com"><span class="primary">link to google</span></a></li></ul></div> <%- include template/footer.ejs -%>');
+        expect(bracks_ejs.trim()).to.equal('<html> <meta charset="utf-8"><br> <body> <% include template/header.ejs %> <div id="divId" class="<%= page %>"><ul style="list-style-type:disc"><li><a href="https://www.google.com"><span class="primary">link to google</span></a></li></ul></div> <%- include template/footer.ejs -%> </body> </html>');
         done();
       }
     });
