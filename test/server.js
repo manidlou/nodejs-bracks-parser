4064bf7d94ee7e1951decc5aee798416dedd2d8d
mawni <mawni@gmail.com>
2016-05-02 05:48:20 -0700

    fix test server

--- test/server.js
+++ test/server.js
@@ -16,11 +16,11 @@ app.set('view engine', 'ejs');
 app.use(bracks_parser('./bracks'));
 
 app.get('/testhtml', function(req, res, next) {
-  fs.createReadStream(path.join(process.cwd(), 'views', 'index-html.html'), 'utf8')
+  fs.createReadStream('./views/index-html.html'), 'utf8')
     .pipe(res);
 });
 
 app.get('/testejs', function(req, res, next) {
-  fs.createReadStream(path.join(process.cwd(), 'views', 'index-ejs.ejs'), 'utf8')
+  fs.createReadStream('./views/index-ejs.ejs'), 'utf8')
     .pipe(res);
 });
